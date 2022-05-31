4006522d7ca61f149a4860afc767cad46596f636
lamres1 <100788211+lamres1@users.noreply.github.com>
2022-05-31 18:29:20 +0530

    Update model.dart

--- lib/weather/model.dart
+++ lib/weather/model.dart
@@ -1,20 +1,25 @@
 import 'package:test_app/weather/page3.dart';
-//import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-//import 'package:flutter/material.dart';
-
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather(
       String? latitude, String? longitude) async {
-    var endpoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=59b8c7ee030fa168174a6a0b2103b1db");
-
-    var response = await http.get(endpoint);
-    var body = jsonDecode(response.body);
-    print(Weather.fromJson(body));
-    return Weather.fromJson(body);
+    if (longitude == "hi") {
+      var endpoint = Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?q=$latitude&appid=59b8c7ee030fa168174a6a0b2103b1db&units=metric");
+      var response = await http.get(endpoint);
+      var body = jsonDecode(response.body);
+      print(Weather.fromJson(body));
+      return Weather.fromJson(body);
+    } else {
+      var endpoint = Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=59b8c7ee030fa168174a6a0b2103b1db&units=metric");
+      var response = await http.get(endpoint);
+      var body = jsonDecode(response.body);
+      print(Weather.fromJson(body));
+      return Weather.fromJson(body);
+    }
   }
 }
